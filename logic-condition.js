66bec7db99155b81bec03cade533a771d5ea6214
asadulR <asad1252001@gmail.com>
2022-01-26 20:14:53 +0600

    another commit

---
+++ logic-condition.js
@@ -0,0 +1,33 @@
+//           if Condition
+
+
+var a = 10;
+var b = 30;
+
+// if (a > b){
+//     console.log("a is greater than b.");
+// }
+// else{
+//     console.log("a is not greater than b.");
+// }
+
+
+
+
+// if (a > b){
+//     console.log("A is greater than B.");
+// }
+// if(a < b){
+//     console.log("B is greater than A.");
+// }
+
+
+
+var n = 5;
+
+if (n % 2 === 0) {
+    console.log(n + " is a even Number.");
+}
+if (n % 2 != 0) {
+    console.log(parseInt(n) + " is a odd number.");
+}
